93dcc7d17b00e35f61a38bfb2491672296131474
Manaila Alice <alicemanaila000@gmail.com>
2025-07-11 16:28:50 +0300

    Add test for logout functionality after successful login

---
+++ cypress/e2e/TesteSauce/LogoutTest.cy.js
@@ -0,0 +1,28 @@
+describe('Logout functionality', () => {
+
+    it('Logs out successfully', () => {
+
+        cy.visit('https://www.saucedemo.com/');
+
+        // Log in with valid credentials
+        cy.get('[data-test="username"]').type('standard_user');
+        cy.get('[data-test="password"]').type('secret_sauce');
+        cy.get('[data-test="login-button"]').click();
+
+        // Check if the user is redirected to the inventory page
+        cy.url().should('include', '/inventory.html');
+
+        // Open the sidebar menu
+        cy.get('#react-burger-menu-btn').click();
+
+        // Click the Logout button in the side menu
+        cy.get('[data-test="logout-sidebar-link"]').click();
+
+        // Check if the login form is visible again
+        cy.get('#login_button_container').should('be.visible');
+
+        // Check if the user is redirected to the login page
+        cy.url().should('eq', 'https://www.saucedemo.com/')
+
+    })
+})
