2b74b9dda821e77d7ef09f09ab3b568ad246370f
Manaila Alice <alicemanaila000@gmail.com>
2025-07-15 16:04:50 +0300

    Optimize 'Back to products button' test

--- cypress/e2e/TesteSauceDemo/BackToProducts.cy.js
+++ cypress/e2e/TesteSauceDemo/BackToProducts.cy.js
@@ -1,24 +1,18 @@
 describe('Back to products button', () => {
 
-    it('Verifies if Back to products button works properly', () => {
-
-        // Visit the website
+    beforeEach(() => {
         cy.visit('https://www.saucedemo.com/');
-
-        // Log in with valid credentials
-        cy.get('[data-test="username"]').type('standard_user');
-        cy.get('[data-test="password"]').type('secret_sauce');
-        cy.get('[data-test="login-button"]').click();
-
-        // Check if the inventory container is visible
+        cy.login('standard_user', 'secret_sauce');
         cy.get('[data-test="inventory-container"]').should('be.visible');
+    });
+
+    it('Verifies if Back to products button works properly', () => {
 
         // Go to the product details page
         cy.get('[data-test="inventory-item-sauce-labs-backpack-img"]').click();
 
         // Check if the Back to products button works
-        cy.get('[data-test="back-to-products"]').should('be.visible');
-        cy.get('[data-test="back-to-products"]').click();
+        cy.get('[data-test="back-to-products"]').should('be.visible').click();
 
         // Check if the URL includes '/inventory.html'
         cy.url().should('include', '/inventory.html'); 
