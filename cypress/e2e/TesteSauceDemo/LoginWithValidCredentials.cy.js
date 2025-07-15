acf5272724b014c99356bc302aebd606c0a88626
Manaila Alice <alicemanaila000@gmail.com>
2025-07-15 14:19:58 +0300

    Improve structure and clarity of valid login test

--- cypress/e2e/TesteSauceDemo/LoginWithValidCredentials.cy.js
+++ cypress/e2e/TesteSauceDemo/LoginWithValidCredentials.cy.js
@@ -2,15 +2,13 @@ describe('Login with valid credentials', () =>{
 
     it('Should log in successfully with valid credentials', () =>{
         cy.visit('https://www.saucedemo.com/');
-        cy.get('[data-test="username"]').type('standard_user');
-        cy.get('[data-test="password"]').type('secret_sauce');
-        cy.get('[data-test="login-button"]').click();
-
-        // Check if the inventory container is visible, indicating a successful login
-        cy.get('[data-test="inventory-container"]').should('be.visible');
+        cy.login('standard_user', 'secret_sauce');
 
         // Check if the user is redirected to the inventory page after login 
         cy.url().should('include', '/inventory.html');
+
+        // Check if the inventory container is visible, indicating a successful login
+        cy.get('[data-test="inventory-container"]').should('be.visible');
         
     })
 })
